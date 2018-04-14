9e41491978163d6d40243154c5f9a628db9a6826
cpatin10 <catapf493@yahoo.com>
2018-04-14 10:02:11 -0500

    ejercicio numero de se por empleado

---
+++ MapReduce/salariosColombianos/numSExEmpleado-mr.py
@@ -0,0 +1,15 @@
+from mrjob.job import MRJob
+
+class SEMeanSalary(MRJob):
+    def mapper1(self, _, line):
+        parameters = line.split(',')
+        yield parameters[1], parameters[0]
+
+    def reducer(self, key, values):
+        se = set()
+        for value in values:
+            se.add(value)
+        yield key, se.len()
+
+if __name__ == '__main__':
+    SEMeanSalary.run()
