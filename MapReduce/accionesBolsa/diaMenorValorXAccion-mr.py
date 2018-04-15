0307424703880c9096e14f3012f09fc57356bb69
cpatin10 <catapf493@yahoo.com>
2018-04-15 10:15:57 -0500

    ejercicio dia de menores valores por accion

---
+++ MapReduce/accionesBolsa/diaMenorValorXAccion-mr.py
@@ -0,0 +1,17 @@
+from mrjob.job import MRJob
+import sys
+
+class leastValueDay(MRJob):
+    def mapper(self, _, line):
+        parameters = line.split(',')
+        value = int(parameters[-2])
+        yield parameters[0], value
+
+    def reducer(self, key, values):
+        leastValue = sys.maxint
+        for value in values:
+            leastValue = min(leastValue, value)
+        yield key, sumSalary/count
+
+if __name__ == '__main__':
+    leastValueDay.run()
