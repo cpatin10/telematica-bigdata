d87f81d2c39d5871d1398c9a4b1093611f888fa0
cpatin10 <catapf493@yahoo.com>
2018-04-17 11:19:48 -0500

    correccion en PeliculasXUsuario-mr.py

--- MapReduce/peliculas/PeliculasXUsuario-mr.py
+++ MapReduce/peliculas/PeliculasXUsuario-mr.py
@@ -10,7 +10,7 @@ class MoviesXUser(MRJob):
         totalMovies = 0
         for value in values:
             totalMovies += 1
-        yield key, totalMovies
+        yield key, values[1]
 
 if __name__ == '__main__':
     MoviesXUser.run()
