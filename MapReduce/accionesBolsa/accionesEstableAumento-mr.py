1c5e3e89da9eee9a232032a00395e580211bf3f3
catalina <catapf493@yahoo.com>
2018-04-17 08:03:52 -0500

    ejercicio de acciones bolsa para determinar acciones que permanecen estables o aumentan

---
+++ MapReduce/accionesBolsa/accionesEstableAumento-mr.py
@@ -0,0 +1,24 @@
+from mrjob.job import MRJob
+import sys
+
+class StableSuperiorActions(MRJob):
+    def mapper(self, _, line):
+        parameters = line.split(',')
+        value = float(parameters[-2])
+        yield parameters[0], value
+
+    def reducer(self, key, values):
+        lastValue = -sys.maxint
+        decreasingAction = false
+        
+        for value in values:
+            if value < lastValue:
+                decreasingAction = True
+                break
+            else
+                lastValue = value
+        if not decreasingAction:
+            yield key, leastValue
+
+if __name__ == '__main__':
+    StableSuperiorActions.run()
