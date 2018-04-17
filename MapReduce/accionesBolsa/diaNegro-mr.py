8feff23feed77e9d0a72e64d099665b9f7929dc8
catalina <catapf493@yahoo.com>
2018-04-17 08:31:06 -0500

    ejercicio dia negro de acciones bolsa

---
+++ MapReduce/accionesBolsa/diaNegro-mr.py
@@ -0,0 +1,33 @@
+from mrjob.job import MRJob
+import sys
+
+class leastValueDay(MRJob):
+    
+    def steps(self):
+        return[
+            MRStep(mapper = self.mapper1,
+                combiner = self.combiner1),
+            MRStep(reducer = self.reducer1)
+        ]
+
+    def mapper1(self, _, line):
+        parameters = line.split(',')
+        value = float(parameters[-2])
+        yield parameters[0], (value, parameters[-1])
+
+    def combiner1(self, key, values):
+        leastValue = sys.maxint
+        leastValueDate = ""
+        for value in values:
+            if value[0] < leastValue:
+                leastValue = value[0]
+                leastValueDate = value[1]
+        yield leastValueDate, 1
+
+    def reducer1(self, key, values):
+        yield key, sum(values)
+
+    
+
+if __name__ == '__main__':
+    leastValueDay.run()
