270611849ba36be72b99799205c11fa137453bd0
catalina <catapf493@yahoo.com>
2018-04-17 07:31:11 -0500

    ejercicio sectores economicos por empleado

---
+++ MapReduce/accionesBolsa/SExEmpleado-mr.py
@@ -0,0 +1,17 @@
+from mrjob.job import MRJob
+import sys
+
+class SExEmployee(MRJob):
+    def mapper(self, _, line):
+        parameters = line.split(',')
+        se = parameters[0]
+        yield parameters[1], se
+
+    def reducer(self, key, values):
+        seSet = set()
+        for value in values:
+            seSet.add(value)
+        yield key, len(seSet)
+
+if __name__ == '__main__':
+    SExEmployee.run()
