2cd41d9656ce9183b3424226eccf5d79fee40fdf
cpatin10 <catapf493@yahoo.com>
2018-04-17 11:29:41 -0500

    ejercicio dia con mayor numero de peliculas vistas de evaluacio peliculas

---
+++ MapReduce/peliculas/diaMasPeliculasVistas-mr.py
@@ -0,0 +1,29 @@
+from mrjob.job import MRJob
+import sys
+
+class GreatestValueDay(MRJob):
+    def steps(self):
+        return[
+            MRStep(mapper = self.mapper1,
+                reducer = self.reducer1),
+            MRStep(reducer = self.reducer2)
+        ]
+
+    def mapper1(self, _, line):
+        parameters = line.split(',')
+        yield parameters[-1], 1
+
+    def reducer1(self, key, values):
+        yield None (key, sum(values))
+
+    def reducer2(self, _, values):
+        movieCount = 0
+        greatestMoviesDay = ""
+        for value in values:
+            if value[1] > movieCount:
+                movieCount = value[1]
+                greatestMoviesDay = value[0]
+        yield greatestMoviesDay, movieCount
+
+if __name__ == '__main__':
+    GreatestValueDay.run()
